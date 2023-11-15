cf3d62b2456899a619dc93d4b96af6b34586c16a
Tomasz Gondek <t.gondek2013@wp.pl>
2023-11-15 17:56:55 +0100

    sniper enemy, + method to make levels w/o much suffering

--- main.py
+++ main.py
@@ -278,6 +278,39 @@ class shotgutBullet(pygame.sprite.Sprite):
         if self.pos[0] > screen.get_width() or self.pos[0] < 0 or self.pos[1] > screen.get_height() or self.pos[1] < 0:
             self.kill()
 
+class sniperBullet(pygame.sprite.Sprite):
+    def __init__(self, x, y, isFriendly, tx, ty):
+        pygame.sprite.Sprite.__init__(self)
+        self.carryDMG = 25
+        self.isFriendly = isFriendly
+        self.pos = (x, y)
+        mx, my = tx, ty
+        self.dir = (mx - x, my - y)
+        length = math.hypot(*self.dir)
+        if length == 0.0:
+            self.dir = (0, -1)
+        else:
+            self.dir = (self.dir[0]/length, self.dir[1]/length)
+        angle = math.degrees(math.atan2(-self.dir[1], self.dir[0]))
+        self.bullet = pygame.image.load('graphics/bullets/Laser_Sprites/03.png')
+        self.bullet = pygame.transform.scale(self.bullet, (200, 32))
+        self.bullet = pygame.transform.rotate(self.bullet, angle)
+        self.speed = 20
+        self.rect = self.bullet.get_rect()
+        self.mask = pygame.mask.from_surface(self.bullet)
+
+    def draw(self, surf):
+        self.rect = self.bullet.get_rect(center=self.pos)
+        surf.blit(self.bullet, self.rect)
+
+    def update(self):
+        self.pos = (self.pos[0] + self.dir[0] * self.speed,
+                    self.pos[1] + self.dir[1] * self.speed)
+        self.rect.move_ip(0,5)
+        self.draw(screen)
+        if self.pos[0] > screen.get_width() or self.pos[0] < 0 or self.pos[1] > screen.get_height() or self.pos[1] < 0:
+            self.kill()
+
 class PerdixNormal(pygame.sprite.Sprite):
     def __init__(self, x, y, tx, ty, scale, speed):
         pygame.sprite.Sprite.__init__(self)
@@ -402,6 +435,68 @@ class PerdixShooter(pygame.sprite.Sprite):
             self.kill()
         self.draw(screen)
 
+class PerdixSniper(pygame.sprite.Sprite):
+    def __init__(self, x, y, scale, speed, targetX, targetY):
+        pygame.sprite.Sprite.__init__(self)
+        self.pos = (x, y)
+        self.image = pygame.image.load('graphics/placeholder_duck.png')
+        self.image = pygame.transform.scale(self.image, (scale, scale))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.speed = speed
+        self.hp = 150
+        self.carryDMG = 20
+        self.Toggle = False
+        self.fireRate = 60
+        self.fireTick = 0
+        self.mask = pygame.mask.from_surface(self.image)
+        self.target = (targetX, targetY)
+
+    def checkColisions(self):
+        hits = []
+        hits = pygame.sprite.spritecollide(self, bullets, 0)
+        for hit in hits:
+            if hit.isFriendly == True:
+                self.hp -= hit.carryDMG
+                hit.kill()
+                dmgP = damagePanel(self.rect.x, self.rect.y, hit.carryDMG, 20, (255, 0, 125))
+                damages.add(dmgP)
+
+    def moveLeftRight(self):
+        if not self.Toggle:
+            self.rect.x += self.speed
+            if self.rect.x >= screen.get_width() - self.image.get_width():
+                self.Toggle = True
+        if self.Toggle == True:
+            self.rect.x -= self.speed
+            if self.rect.x <= 0:
+                self.Toggle = False
+
+    def shoot(self):
+        self.fireTick += 1
+        if self.fireTick >= self.fireRate:
+            bullet = sniperBullet(self.rect.centerx, self.rect.centery+30, False, self.target[0], self.target[1])
+            bullets.add(bullet)
+            self.fireTick = 0
+
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+    def update(self):
+        self.checkColisions()
+        self.shoot()
+        self.target = (theCat.rect.centerx, theCat.rect.centery)
+        if self.rect.y > screen.get_height():
+            self.kill()
+        # self.moveDown()
+        self.moveLeftRight()
+        if self.hp <= 0:
+            explosion = Explosion2Firework(self.rect.centerx, self.rect.centery, 250)
+            explosions.add(explosion)
+            self.kill()
+        self.draw(screen)
+
 class damagePanel(pygame.sprite.Sprite):
     def __init__(self, x, y, dmg, counter, color):
         pygame.sprite.Sprite.__init__(self)
@@ -488,6 +583,7 @@ class Game:
         self.levelIntroDone = int(saveState[0])
         self.level01Done = int(saveState[1])
         self.currentLevel = self.level
+        self.secCounter = 0
 
     def stageManager(self):
         if self.level == "menu":
@@ -672,12 +768,42 @@ class Game:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-            if event.type == createEnemyEvent:
-                partridgesNormal.add(PerdixNormal(random.randrange(100, 1100), -100, random.randrange(100, 1100), 1000, 100, 5))
-                partridgesNormal.add(PerdixShooter(-100, 100, 100, 3))
+            if event.type == oneSec:
+                self.secCounter += 1
+                print(self.secCounter)
+                if self.secCounter == 1:
+                    partridgesNormal.add(PerdixNormal(0, -100, 300, 1000, 100, 2))
+                    partridgesNormal.add(PerdixNormal(500, -200, 400, 1000, 100, 2))
+                    partridgesNormal.add(PerdixNormal(800, -200, 500, 1000, 100, 2))
+                    partridgesNormal.add(PerdixNormal(200, -200, 600, 1000, 100, 2))
+                    partridgesNormal.add(PerdixNormal(35, -300, 700, 1000, 100, 2))
+                    partridgesNormal.add(PerdixNormal(178, -400, 800, 1000, 100, 2))
+                    partridgesNormal.add(PerdixNormal(1100, -500, 900, 1000, 100, 2))
+                if self.secCounter == 8:
+                    partridgesNormal.add(PerdixShooter(-100, 200, 100, 3))
+                    partridgesNormal.add(PerdixShooter(1300, 100, 100, 3))
+                if self.secCounter == 10:
+                    for x in range(10):
+                        partridgesNormal.add(PerdixNormal(random.randrange(0, 1200), random.randrange(100, 800) * -1,
+                                                          random.randrange(0, 1200), 1000, 100, random.randrange(2, 3)))
+                if self.secCounter == 12:
+                    for x in range(13):
+                        partridgesNormal.add(PerdixNormal(random.randrange(0, 1200), random.randrange(100, 800) * -1,
+                                                          random.randrange(0, 1200), 1000, 100, random.randrange(2, 3)))
+                if self.secCounter == 15:
+                    partridgesNormal.add(PerdixShooter(-100, 200, 100, 3))
+                    partridgesNormal.add(PerdixShooter(1300, 100, 100, 3))
+                    partridgesNormal.add(PerdixShooter(1300, 300, 100, 3))
+                if self.secCounter == 18:
+                    partridgesNormal.add(PerdixSniper(-100, 200, 100, 3, theCat.rect.x, theCat.rect.y))
+                    partridgesNormal.add(PerdixSniper(1300, 80, 100, 3, theCat.rect.x, theCat.rect.y))
+
+
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     self.level = 'pause'
+
         theCat.updateSprite(screen)
         partridgesNormal.update()
         bullets.update()
@@ -694,12 +820,12 @@ partridgesNormal = pygame.sprite.Group()
 explosions = pygame.sprite.Group()
 theCat = CAT((screen.get_width()/2-32, 700))
 game = Game()
-
 # Custom events
-createEnemyEvent, t = pygame.USEREVENT+1, 1000
-pygame.time.set_timer(createEnemyEvent, t)
+oneSec, t = pygame.USEREVENT+1, 1000
+pygame.time.set_timer(oneSec, t)
 
 # main loop
 while True:
+    secCounter = 0;
     game.stageManager()
 
